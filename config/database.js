f91699a7bfd84491937314c2d436074d12c06f9a
MandarUmare <90469953+MandarUmare@users.noreply.github.com>
2024-07-25 23:35:25 +0530

    All Done

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('employees_db', 'root', 'awaze_shaikh', {
-  host: '127.0.0.1',
-  dialect: 'mysql'
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host:process.env.DB_HOST,
+  dialect: process.env.DB_HOST,
 });
 
 module.exports = sequelize;
--- models/index.js
+++ models/index.js
@@ -3,9 +3,9 @@
 const { Sequelize } = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.json')[env];
-const configEnv = config[env];
-const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
-  host: configEnv.host,
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host:process.env.DB_HOST,
   dialect: process.env.DB_HOST,
   logging: false, // Set to true to see SQL queries in console
 });
